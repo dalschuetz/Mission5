7d94d04a244b71b3afd82c467b09d3ba666a37c1
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-02-03 13:21:21 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$("#btnSubmit").click(function () {
+
+    let hours = $("#Hours").val()
+
+    if ((/^\d+$/.test(hours))){
+        $("#AnswerText").removeAttr("hidden");
+        $("#Answer").removeAttr("hidden");
+        $("#Answer").val("$" + hours * 20);
+    }
+    else {
+        alert("Please enter a positive integer for: Hours");
+    }
+})
