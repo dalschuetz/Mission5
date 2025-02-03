746f53a726de264c88fe5f8f2dd59881dbb2f2b2
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-02-03 13:30:39 -0700

    notes and readability

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -2,10 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//Submit button functionality
 $("#btnSubmit").click(function () {
 
     let hours = $("#Hours").val()
 
+    //checks if hours is an integer and if it is null
     if ((/^\d+$/.test(hours))){
         $("#AnswerText").removeAttr("hidden");
         $("#Answer").removeAttr("hidden");
